20579372f186ebe3c62946cc0418a07ec2d41f46
Mahdi Esmailiyan <esmailiyan.mahdi@gmail.com>
2023-09-03 19:18:06 +0330

    Config inputs in logger

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import os
 import logging
 
-def log(data:str) -> bool:
+def log(title:str, data:str) -> bool:
     # Set the logging configuration
     log_file = '.log'
-    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(message)s')
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='- %(message)s')
 
     # Log some data
-    logging.info(data)
+    logging.info(f"{title}: {data}")
