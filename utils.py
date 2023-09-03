12cadb106ae39503e2f1b2fbfd1d4400f1e33cfb
Mahdi Esmailiyan <esmailiyan.mahdi@gmail.com>
2023-09-03 18:37:20 +0330

    Add time to logger

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import logging
 def log(data:str) -> bool:
     # Set the logging configuration
     log_file = '.log'
-    logging.basicConfig(filename=log_file, level=logging.INFO, format='- %(message)s')
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(message)s')
 
     # Log some data
     logging.info(data)
