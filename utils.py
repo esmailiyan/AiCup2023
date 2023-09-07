6b446165e5c2841ed1acba0d959cbad44edd39c1
Mahdi Esmailiyan <esmailiyan.mahdi@gmail.com>
2023-09-07 17:45:59 +0330

    Edit logger

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import os
 import logging
 
-def log(title:str, data:str) -> bool:
+def log(data:str) -> bool:
     # Set the logging configuration
     log_file = '.log'
-    logging.basicConfig(filename=log_file, level=logging.INFO, format='- %(message)s')
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(message)s')
 
     # Log some data
-    logging.info(f"{title}: {data}")
+    logging.info(data)
 
 '''
     Tasks: 
